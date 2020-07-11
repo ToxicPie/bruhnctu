adeaf1259a5d4786175cb52da3276ba5542f8829
toxicpie <kent05011607@gmail.com>
2020-07-11 14:45:48 +0800

    style updates

--- config/flask_config.py
+++ config/flask_config.py
@@ -1,7 +1,7 @@
 DEBUG = False
 TEMPLATES_AUTO_RELOAD = True
 
-FLATPAGES_MARKDOWN_EXTENSIONS = ['fenced_code', 'codehilite', 'toc', 'components.md_extras']
+FLATPAGES_MARKDOWN_EXTENSIONS = ['fenced_code', 'codehilite', 'toc', 'tables', 'components.md_extras']
 FLATPAGES_EXTENSION = ['.htm', '.html', '.md']
 FLATPAGES_AUTO_RELOAD = True
 FLATPAGES_EXTENSION_CONFIGS = {
